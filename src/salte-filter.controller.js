effd6bf08ed1ae45a79c0dcdc830ebcb1dbf17f2
Nick <nick@salte.io>
2016-02-06 20:32:36 -0600

    feat(): Initial Commit

---
+++ src/salte-filter.controller.js
@@ -0,0 +1,50 @@
+export default class SalteFilter {
+  constructor() {
+    this.filters = [];
+    this.listeners = [];
+  }
+
+  $onChanges(changes) {
+    if (changes.data) {
+      this.filter();
+    }
+  }
+
+  filter() {
+    this.filteredData = [];
+    if (this.data) {
+      this.data.forEach((data) => {
+        let valid = true;
+        this.filters.forEach((filter) => {
+          if (!filter(data)) {
+            valid = false;
+          }
+        });
+        if (valid) {
+          this.filteredData.push(data);
+        }
+      });
+    }
+    this.listeners.forEach((listener) => listener(this.filteredData));
+  }
+
+  addFilter(filter) {
+    if (this.filters.indexOf(filter) === -1) {
+      this.filters.push(filter);
+      this.filter();
+    }
+  }
+
+  addFilterListener(listener) {
+    if (!listener) return;
+    this.listeners.push(listener);
+  }
+
+  removeFilter(filter) {
+    const index = this.filters.indexOf(filter);
+    if (index !== -1) {
+      this.filters.splice(index, 1);
+      this.filter();
+    }
+  }
+}
