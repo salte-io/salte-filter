1e59d5565480f1757ecdc04ee3cfe2827022f803
Ceci <admin@cecilias.me>
2018-01-27 20:14:45 -0600

    fix: switched to mocha

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function(config) {
     basePath: '',
 
     frameworks: [
-      'jasmine',
+      'mocha',
       'sinon'
     ],
 
--- tests/salte-filter/salte-filter.spec.js
+++ tests/salte-filter/salte-filter.spec.js
@@ -1,3 +1,5 @@
+import { expect } from 'chai';
+
 import salteFilter from '../../src/salte-filter.module.js';
 
 describe('component(salte-filter)', () => {
@@ -21,9 +23,9 @@ describe('component(salte-filter)', () => {
 
   describe('function(filter)', () => {
     it('should return a dereferenced version of the original data if no filters are defined', () => {
-      expect(scope.filteredData).toEqual(scope.data);
+      expect(scope.filteredData).to.deep.equal(scope.data);
       scope.filteredData.push({ business: 'Valve' });
-      expect(scope.filteredData).not.toEqual(scope.data);
+      expect(scope.filteredData).not.to.deep.equal(scope.data);
     });
 
     it('should filter when a filter is added or removed', () => {
@@ -31,61 +33,61 @@ describe('component(salte-filter)', () => {
         return data.business === 'Salte';
       }
 
-      expect(scope.filteredData).toEqual(scope.data);
+      expect(scope.filteredData).to.deep.equal(scope.data);
       controller.addFilter(filter);
       scope.$digest();
-      expect(scope.filteredData).toEqual([{ business: 'Salte' }]);
+      expect(scope.filteredData).to.deep.equal([{ business: 'Salte' }]);
       controller.removeFilter(filter);
       scope.$digest();
-      expect(scope.filteredData).toEqual(scope.data);
+      expect(scope.filteredData).to.deep.equal(scope.data);
     });
 
     it('should filter when the input data is modified', () => {
-      expect(scope.filteredData).toEqual(scope.data);
+      expect(scope.filteredData).to.deep.equal(scope.data);
       scope.data = [{ restaurant: 'Taco Bell' }];
       scope.$digest();
-      expect(scope.filteredData).toEqual(scope.data);
+      expect(scope.filteredData).to.deep.equal(scope.data);
     });
 
     it('should not filter when an individual element is pushed or popped from the input data', () => {
-      expect(scope.filteredData).toEqual(scope.data);
+      expect(scope.filteredData).to.deep.equal(scope.data);
       scope.data.push({ business: 'Twitch' });
       scope.$digest();
-      expect(scope.filteredData).toEqual([{ business: 'Salte' }, { business: 'Google' }]);
+      expect(scope.filteredData).to.deep.equal([{ business: 'Salte' }, { business: 'Google' }]);
     });
 
     it('should allow the add a filter listener', () => {
       const listener = sinon.spy();
       controller.addFilterListener(listener);
       controller.filter();
-      expect(listener.callCount).toEqual(1);
+      expect(listener.callCount).to.deep.equal(1);
       controller.removeFilterListener(listener);
       controller.filter();
-      expect(listener.callCount).toEqual(1);
+      expect(listener.callCount).to.deep.equal(1);
     });
   });
 
   describe('binding(onFilter)', () => {
     it('should call onFilter initially', () => {
-      expect(scope.onFilter.callCount).toEqual(1);
-      expect(scope.onFilter.calledWith(scope.filteredData)).toEqual(true);
+      expect(scope.onFilter.callCount).to.deep.equal(1);
+      expect(scope.onFilter.calledWith(scope.filteredData)).to.deep.equal(true);
     });
 
     it('should call onFilter when filter() is called', () => {
-      expect(scope.onFilter.callCount).toEqual(1);
-      expect(scope.onFilter.calledWith(scope.filteredData)).toEqual(true);
+      expect(scope.onFilter.callCount).to.deep.equal(1);
+      expect(scope.onFilter.calledWith(scope.filteredData)).to.deep.equal(true);
       controller.filter();
-      expect(scope.onFilter.callCount).toEqual(2);
-      expect(scope.onFilter.calledWith(scope.filteredData)).toEqual(true);
+      expect(scope.onFilter.callCount).to.deep.equal(2);
+      expect(scope.onFilter.calledWith(scope.filteredData)).to.deep.equal(true);
     });
 
     it('should call onFilter when data is changed', () => {
-      expect(scope.onFilter.callCount).toEqual(1);
-      expect(scope.onFilter.calledWith(scope.filteredData)).toEqual(true);
+      expect(scope.onFilter.callCount).to.deep.equal(1);
+      expect(scope.onFilter.calledWith(scope.filteredData)).to.deep.equal(true);
       scope.data = [{ restaurant: 'Taco Bell' }];
       scope.$digest();
-      expect(scope.onFilter.callCount).toEqual(2);
-      expect(scope.onFilter.calledWith(scope.filteredData)).toEqual(true);
+      expect(scope.onFilter.callCount).to.deep.equal(2);
+      expect(scope.onFilter.calledWith(scope.filteredData)).to.deep.equal(true);
     });
   });
 });
